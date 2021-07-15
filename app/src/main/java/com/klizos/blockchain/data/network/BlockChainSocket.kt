47d15d6abd96b8aaaffc359fe71e865a92dd228c
Ahmad Jawid <ahmad@klizos.com>
2021-07-15 21:45:15 +0530

    initial commit

---
+++ app/src/main/java/com/klizos/blockchain/data/network/BlockChainSocket.kt
@@ -0,0 +1,9 @@
+package com.klizos.blockchain.data.network
+
+/**
+ * Created by Ahmad Jawid Muhammadi
+ * on 15-07-2021.
+ */
+
+class BlockChainSocket {
+}
---
+++ app/src/main/java/com/klizos/blockchain/ui/FirstFragment.kt
@@ -0,0 +1,29 @@
+package com.klizos.blockchain.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.klizos.blockchain.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
